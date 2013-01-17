2799f630a64327c8e3928d6c918a65399ac1e61f
= <jkiefner@gmail.com>
2013-01-16 20:20:10 -0500

    missed these unversioned files.

---
+++ DMAssist/DMAssistCore/DMAssistCore/Dice/TenSidedDie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMAssistCore.Dice
+{
+	public class TenSidedDie : Die
+	{
+
+		
+		public TenSidedDie()
+			:base()
+		{ }
+
+		public TenSidedDie(Random randomGenerator)
+			: base(randomGenerator)
+		{
+			_randomGenerator = randomGenerator;
+		}
+
+		private int _dieRoll = 0;
+		public override int DieRoll
+		{
+			get
+			{
+				if (_dieRoll == 0)
+				{
+					_dieRoll = _randomGenerator.Next(1, 11);
+					return _dieRoll;
+				}
+				else
+				{
+					return _dieRoll;
+				}
+
+			}
+		}
+	}
+}
