093e4c7dbd9b2574310cd55455dec55ce94a0b2d
= <jkiefner@gmail.com>
2013-01-16 19:14:11 -0500

    added beginning of UI layer

---
+++ DMAssist/DMAssistCore/DMAssistCore/Enums/DiceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMAssistCore.Enums
+{
+	public enum DiceType
+	{
+		SixSidedDie = 0,
+		EightSidedDie = 1,
+		TenSidedDie = 2,
+		TwelveSidedDie = 3,
+		TwentySidedDie = 4
+	}
+}
